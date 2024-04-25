a9a1dd5bf8d346cdb849b3d447684fe8a3d956eb
Akshara Sharma <akshara582582@gmail.com>
2024-04-25 13:44:20 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,13 +1,6 @@
 
 
 function smallestMissingPositiveInteger(nums) {
-  const set = new Set(nums.filter(num => num > 0));
-  for (let i = 1; i <= set.size + 1; i++) {
-      if (!set.has(i)) return i;
-  }
-}
-
-function smallestMissingPositiveInteger(nums) {
   if (nums.length === 0) {
       return 1;
   }
