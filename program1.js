1519508364e313054a77424782a83006141ff533
Akshara Sharma <akshara582582@gmail.com>
2024-04-25 13:46:21 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,20 +1,20 @@
 
-function longestSubstring(s) {
-    let longestLength = 0;
-    let seen = new Set();
-    let left = 0;
+// function longestSubstring(s) {
+//     let longestLength = 0;
+//     let seen = new Set();
+//     let left = 0;
 
-    for (let right = 0; right < s.length; right++) {
-        while (seen.has(s[right])) {
-            seen.delete(s[left]);
-            left++;
-        }
-        seen.add(s[right]);
-        longestLength = Math.max(longestLength, right - left + 1);
-    }
+//     for (let right = 0; right < s.length; right++) {
+//         while (seen.has(s[right])) {
+//             seen.delete(s[left]);
+//             left++;
+//         }
+//         seen.add(s[right]);
+//         longestLength = Math.max(longestLength, right - left + 1);
+//     }
 
-    return longestLength;
-}
+//     return longestLength;
+// }
 
 
 
