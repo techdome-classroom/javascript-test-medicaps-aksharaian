70f874718a5e8890184ac485070d9cc69e099cd0
Akshara Sharma <akshara582582@gmail.com>
2024-04-25 13:42:34 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,7 +1,41 @@
+
 function longestSubstring(s) {
-    // Implementation of longestSubstring function
+    let longestLength = 0;
+    let seen = new Set();
+    let left = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (seen.has(s[right])) {
+            seen.delete(s[left]);
+            left++;
+        }
+        seen.add(s[right]);
+        longestLength = Math.max(longestLength, right - left + 1);
+    }
+
+    return longestLength;
 }
 
+
+
+function longestSubstringLength(s) {
+    if (s.length === 0) {
+        return 0;
+    }
+    
+    let maxLength = 0;
+    let start = 0;
+    const charIndexMap = {};
+    
+    for (let end = 0; end < s.length; end++) {
+        if (s[end] in charIndexMap) {
+            start = Math.max(start, charIndexMap[s[end]] + 1);
+        }
+        
+        charIndexMap[s[end]] = end;
+        maxLength = Math.max(maxLength, end - start + 1);
+    }
+    
+    return maxLength;
+}
 module.exports = { longestSubstring };
-
-
--- program2.js
+++ program2.js
@@ -1,7 +1,11 @@
+
+
 function smallestMissingPositiveInteger(nums) {
-
-  // Implement the function smallest_missing_positive_integer
-
+  const set = new Set(nums.filter(num => num > 0));
+  for (let i = 1; i <= set.size + 1; i++) {
+      if (!set.has(i)) return i;
+  }
 }
+
 
 module.exports = smallestMissingPositiveInteger;
